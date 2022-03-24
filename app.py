ba0dbc9b488fe5fdfdf582775d410dcd576bfc93
Max <maxs987@gmail.com>
2022-03-24 18:46:48 +0300

    more logs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 import os
 from datetime import timedelta
 import argparse
+from logging.handlers import TimedRotatingFileHandler
 
 from subscriber.bot import make_updater
 
@@ -13,6 +14,10 @@ UPDATE_DELTA = timedelta(hours=args.crawler)
 token = os.environ['TOKEN']
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger('subscriber')
+handler = TimedRotatingFileHandler(f"{os.environ['LOGS_PATH']}/warning.log", when='midnight')
+handler.setLevel(logging.WARNING)
+logger.addHandler(handler)
 
 updater = make_updater(token, update_interval=args.notifier, crawler_interval=UPDATE_DELTA.total_seconds())
 updater.start_polling()
