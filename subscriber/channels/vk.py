7ace304e338a86fcf988235aefb5545b18dd5f8f
Max <maxs987@gmail.com>
2022-04-11 20:44:37 +0300

    removed headers

--- subscriber/channels/vk.py
+++ subscriber/channels/vk.py
@@ -14,13 +14,6 @@ class VK(ChannelAdapter):
 
     GROUP_NAME = re.compile(r'^/(\w+)$', flags=re.IGNORECASE)
     IMAGE_LINK = re.compile(r'background-image: url\((.*)\);')
-    REQUEST_HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en,en-US;q=0.7,ru;q=0.3',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Cache-Control': 'max-age=0',
-    }
 
     def track(self, url: str) -> ChannelData:
         path = urlparse(url).path
@@ -30,7 +23,7 @@ class VK(ChannelAdapter):
         return ChannelData(url, name.group(1))
 
     def update(self, update_url: str, channel: ChannelData) -> Iterable[PostUpdate]:
-        doc = html.fromstring(requests.get(update_url, headers=VK.REQUEST_HEADERS).content.decode('utf-8'))
+        doc = html.fromstring(requests.get(update_url).content.decode('utf-8'))
         for element in reversed(doc.cssselect('.wall_post_cont')):
             i = element.attrib.get('id', '')
             if i.startswith('wpt-'):
@@ -38,7 +31,7 @@ class VK(ChannelAdapter):
                 yield PostUpdate(i, f'https://vk.com/wall-{i}')
 
     def scrape(self, post_url: str) -> Content:
-        doc = html.fromstring(requests.get(post_url, headers=VK.REQUEST_HEADERS).content.decode('utf-8'))
+        doc = html.fromstring(requests.get(post_url).content.decode('utf-8'))
         _, i = post_url.split('/wall-')
         post, = [x for x in doc.cssselect(f'div[data-post-id="-{i}"]')]
         kw = {}
